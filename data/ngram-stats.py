dc49bc43c26edfe735439b9e5c39a612fe98509e
Camillo Lugaresi <camillo.lugaresi@gmail.com>
2011-10-09 06:08:26 -0500

    statistics = hard

---
+++ data/ngram-stats.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+import fileinput
+import re
+from collections import defaultdict
+from operator import itemgetter
+import os
+from math import sqrt
+from scipy.special import stdtr
+
+transcript_dir = 'transcripts'
+ngram_dir = 'ngrams'
+
+def welch(mean1, var1, size1, mean2, var2, size2):
+	t = (mean1 - mean2) / sqrt(var1/size1 + var2/size2)
+	nu = (var1/size1 + var2/size2)**2 / ( var1**2/( size1**2 * (size1-1) ) + var2**2/( size2**2 * (size2-1) ) )
+	return nu, t
+
+class NgramDetails(object):
+	def __init__(self):
+		self.loadNgrams()
+		self.character_ngrams = {}
+		self.character_ngram_totals = {}
+	
+	def loadNgrams(self):
+		self.ngrams = {}
+		self.ngram_totals = {}
+		for file_name in os.listdir(ngram_dir):
+			m = re.match("futurama-(\\d+)-grams.txt", file_name)
+			if not m: continue
+			n = int(m.group(1))
+			file_path = os.path.join(ngram_dir, file_name)
+			with open(file_path) as f:
+				total_count = 0
+				self.ngrams[n] = {}
+				for line in f:
+					if total_count == 0:
+						total_count = int(line)
+						continue
+					count, text = line.split('\t')
+					words = tuple(text.split())
+					assert len(words) == n
+					self.ngrams[n][words] = {'count':int(count), 'occurrences':[]}
+				self.ngram_totals[n] = total_count
+	
+	def process_line(self, season, episode, lineno, characters, text):
+#		words = [w.strip('.!?,;:').lower() for w in text.split()]
+		words = tuple([w.strip('.!?,;:').lower() for w in re.split('[\s,\.!?;:"]+', text) if w != ''])
+		characters = characters.split(';')
+		for n in xrange(1, len(words) + 1):
+			for i in xrange(len(words) + 1 - n):
+				w = tuple(words[i:i+n])
+				try:
+					ng = self.ngrams[n][w]
+				except KeyError:
+					continue	# it is not one of the chosen
+				ng['occurrences'].append((season, episode, lineno))
+				for c in characters:
+					if c not in self.character_ngrams:
+						self.character_ngrams[c] = defaultdict(lambda:{'count':0, 'occurrences':[]})
+						self.character_ngram_totals[c] = defaultdict(int)
+					cn = self.character_ngrams[c][w]
+					cn['count'] += 1
+					cn['occurrences'].append((season, episode, lineno))
+					self.character_ngram_totals[c][n] += 1
+
+	def character_stats(self):
+		for char, cngr in self.character_ngrams.items():
+			for words, ng in cngr.items():
+				n = len(words)
+				count_all = self.ngrams[n][words]['count']
+				count_char = ng['count']
+				
+				# bernoulli!
+				char_total = float(self.character_ngram_totals[char][n])
+				char_p = count_char / char_total
+				other_total = self.ngram_totals[n] - char_total
+				other_p = (count_all - count_char) / other_total
+				if char_p == 1.0:
+					p_value = 1.0		# LOLWUT
+				else:
+					nu, t = welch(char_p, char_p*(1.0-char_p), char_total, other_p, other_p*(1.0-other_p), other_total)
+					p_value = stdtr(nu, t)
+				print char, words, "count:", count_char, "total:",char_total, other_p, nu, t, p_value
+
+if __name__ == "__main__":
+	ngrams = NgramDetails()
+	for season_name in os.listdir(transcript_dir):
+		m = re.match("S(\\d+)", season_name)
+		if not m: continue
+		season = int(m.group(1))
+		season_dir = os.path.join(transcript_dir, season_name)
+		for ep_file_name in os.listdir(season_dir):
+			m = re.match("S\\d+E(\\d+)", ep_file_name)
+			if not m: continue
+			episode = int(m.group(1))
+			ep_file_path = os.path.join(season_dir, ep_file_name)
+			print ep_file_name
+			with open(ep_file_path) as f:
+				for lineno, line in enumerate(f):
+					timestamp, character, dialogue = line.split("\t")
+					
+					dialogue = re.sub('\[[^]]+\]', '', dialogue)
+					ngrams.process_line(season, episode, lineno, character, dialogue)
+	
+	ngrams.character_stats()
+
+print ngrams.character_ngrams['Bender'][('bite', 'my', 'shiny', 'metal', 'ass')]
--- data/ngram.py
+++ data/ngram.py
@@ -6,22 +6,28 @@ from operator import itemgetter
 import os
 
 transcript_dir = 'transcripts'
+out_dir = 'ngrams'
+
+try: os.mkdir(out_dir)
+except os.error: pass
 
 # NOTE: this is hideously inefficient. I know how to make it efficient,
 # but I don't wanna.
 
 class NgramCounter(object):
 	def __init__(self):
-		self.ngram_counts = defaultdict(int)
+		self.ngram_counts = defaultdict(int)	# ('a','tuple') -> # of occurrences
+		self.ngram_totals = defaultdict(int)	# n -> total # of n-grams
 	
 	def process(self, text):
 #		words = [w.strip('.!?,;:').lower() for w in text.split()]
 		words = [w.strip('.!?,;:').lower() for w in re.split('[\s,\.!?;:"]+', text) if w != '']
-		for n in xrange(2, len(words) + 1):
+		for n in xrange(1, len(words) + 1):
 			for i in xrange(len(words) + 1 - n):
 				self.ngram_counts[tuple(words[i:i+n])] += 1
+				self.ngram_totals[n] += 1
 	
-	def report(self):
+	def prune(self):
 		print len(self.ngram_counts)
 		print "pruning..."
 		for w, n in self.ngram_counts.items():
@@ -39,25 +45,39 @@ class NgramCounter(object):
 					if subw in self.ngram_counts and self.ngram_counts[subw] == n:
 						del self.ngram_counts[subw]
 		print len(self.ngram_counts)
-		
-		ngram_list = [(len(w), n, w) for w, n in self.ngram_counts.items()]
+	
+	def report(self, out_prefix):
+		# maxn = max(len(w) for w in self.ngram_counts.keys())
+		ngram_lists = defaultdict(list)
+		for w, count in self.ngram_counts.items():
+			ngram_lists[len(w)].append((w, count))
 		print "sorting..."
-		ngram_list.sort(key=itemgetter(1), reverse=True)
-		ngram_list.sort(key=itemgetter(0), reverse=True)
-		for l,n,w in ngram_list:
-			print l, n, w
+		for n, ngram_list in ngram_lists.items():
+			ngram_list.sort(key=itemgetter(1), reverse=True)
+		print "writing..."
+		for n, ngram_list in ngram_lists.items():
+			out_name = out_prefix + "-%d-grams.txt" % n
+			print out_name
+			with open(out_name, "w") as out:
+				out.write("%d\n" % self.ngram_totals[n])
+				for w, count in ngram_list:
+					out.write("%d\t%s\n" % (count, " ".join(w)))
 
-ngram = NgramCounter()
-for season_name in os.listdir(transcript_dir):
-	season_dir = os.path.join(transcript_dir, season_name)
-	for ep_file_name in os.listdir(season_dir):
-		ep_file_path = os.path.join(season_dir, ep_file_name)
-		print ep_file_name
-		with open(ep_file_path) as f:
-			for line in f:
-				timestamp, character, dialogue = line.split("\t")
-				
-				dialogue = re.sub('\[[^]]+\]', '', dialogue)
-				ngram.process(dialogue)
-print "summary..."
-ngram.report()
+if __name__ == "__main__":
+	ngram = NgramCounter()
+	for season_name in os.listdir(transcript_dir):
+		if not re.match("S\\d+", season_name): continue
+		season_dir = os.path.join(transcript_dir, season_name)
+		for ep_file_name in os.listdir(season_dir):
+			if not re.match("S\\d+E\\d+", ep_file_name): continue
+			ep_file_path = os.path.join(season_dir, ep_file_name)
+			print ep_file_name
+			with open(ep_file_path) as f:
+				for line in f:
+					timestamp, character, dialogue = line.split("\t")
+					
+					dialogue = re.sub('\[[^]]+\]', '', dialogue)
+					ngram.process(dialogue)
+	print "summary..."
+	ngram.prune()
+	ngram.report(os.path.join(out_dir,"futurama"))
